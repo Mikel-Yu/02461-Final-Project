27bda64e8ada24c3635db0e8479e1145c300de3d
Mikel <113314077+Mikel-Yu@users.noreply.github.com>
2024-01-04 15:57:45 +0100

    Add files via upload

--- webcam_test.py
+++ webcam_test.py
@@ -23,46 +23,53 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               ]
 
 
+
+frame_counter = 0
+
 while True:
     success, img = cap.read()
-    results = model(img, stream=True)
+    
+    frame_counter += 1
+    
+    if frame_counter % 30 == 0: # Analyze every x'th frame
+        results = model(img, stream=True)
 
         # coordinates
-    for r in results:
-        boxes = r.boxes
+        for r in results:
+            boxes = r.boxes
 
-        for box in boxes:
-            # bounding box
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+            for box in boxes:
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-            # Calculate width and height
-            width = x2 - x1
-            height = y2 - y1
+                # Calculate width and height
+                width = x2 - x1
+                height = y2 - y1
 
-            # put box in cam
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                # put box in cam
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-            # confidence
-            confidence = math.ceil((box.conf[0]*100))/100
-            print(f"Confidence: {confidence}")
+                # confidence
+                confidence = math.ceil((box.conf[0]*100))/100
+                print(f"Confidence: {confidence}")
 
-            # class name
-            cls = int(box.cls[0])
-            print(f"Class name: {classNames[cls]}")
+                # class name
+                cls = int(box.cls[0])
+                print(f"Class name: {classNames[cls]}")
 
-            # Print coordinates and size
-            print(f"Coordinates: ({x1}, {y1}), ({x2}, {y2})")
-            print(f"Width: {width}, Height: {height}")
+                # Print coordinates and size
+                print(f"Coordinates: ({x1}, {y1}), ({x2}, {y2})")
+                print(f"Width: {width}, Height: {height}")
 
-            # object details
-            org = [x1, y1]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            fontScale = 1
-            color = (255, 0, 0)
-            thickness = 2
+                # object details
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
 
-            cv2.putText(img, f"{classNames[cls]}: {confidence}", org, font, fontScale, color, thickness)
+                cv2.putText(img, f"{classNames[cls]}: {confidence}", org, font, fontScale, color, thickness)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
