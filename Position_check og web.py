84eb4c61c384ecf4d318b073493c636b86abd03e
karliboo <155446362+karliboo@users.noreply.github.com>
2024-01-05 13:42:19 +0100

    Add files via upload

---
+++ Position_check og web.py
@@ -0,0 +1,99 @@
+from ultralytics import YOLO
+import cv2
+import math 
+import keyboard
+# start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+# model
+model = YOLO("yolo-Weights/yolov8n.pt")
+
+# object classes
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+def position_check(a,b):
+    for i in range(len(a)):
+        different = True
+        for c in range(len(b)):
+            if abs(a[i][0] - b[c][0]) > 100:
+                print(1)
+            elif abs(a[i][1] - b[c][1]) > 100:
+                print(2)
+            elif abs(a[i][2] - b[c][2]) > 100:
+                print(3)
+            elif abs(a[i][3] - b[c][3]) > 100:
+                print(4)
+            else:
+                different = False
+                break
+        if different == True:
+            return different
+
+frame_counter = 0
+new_list = []
+while True:
+    success, img = cap.read()
+    
+    frame_counter += 1
+    
+    if frame_counter % 120 == 0: # Analyze every x'th frame
+        results = model(img, stream=True)
+
+        # coordinates
+        for r in results:
+            boxes = r.boxes
+            old_list = new_list
+            new_list = []
+            for box in boxes:
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+                # Calculate width and height
+                width = x2 - x1
+                height = y2 - y1
+
+                # put box in cam
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+                # confidence
+                confidence = math.ceil((box.conf[0]*100))/100
+                print(f"Confidence: {confidence}")
+
+                # class name
+                cls = int(box.cls[0])
+                print(f"Class name: {classNames[cls]}")
+
+                # Print coordinates and size
+                print(f"Coordinates: ({x1}, {y1}), ({x2}, {y2})")
+                print(f"Width: {width}, Height: {height}")
+
+                # object details
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+
+                cv2.putText(img, f"{classNames[cls]}: {confidence}", org, font, fontScale, color, thickness)
+                new_list.append([x1, y1, x2, y2,classNames[cls]])
+
+        position_check(new_list,old_list) #opdager ikke hvis et piece er forsvundet hvis der ikke er sket andre ændringer
+    cv2.imshow('Webcam', img)
+    if keyboard.is_pressed('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
