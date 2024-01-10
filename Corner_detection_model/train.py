803e5b99819f7151206d3f488f237ece88516915
poulguoskov <80295985+poulguoskov@users.noreply.github.com>
2024-01-10 21:43:07 +0100

    Update train.py

--- Corner_detection_model/train.py
+++ Corner_detection_model/train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8s.pt')  # load a pretrained model (recommended for training)
 
 # Train the model with 2 GPUs
-results = model.train(data='/Users/p/02.DTU/1.S/Intelligente systemer/chess/chess_data/data.yaml', epochs=150, imgsz=640, device='cpu')
+results = model.train(data='/Users/p/02.DTU/1.S/Intelligente systemer/chess/chess_data/data.yaml', epochs=100, imgsz=640)
